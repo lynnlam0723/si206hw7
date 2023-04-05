30b22c385ab03d7b81dffa522c7d603307ca29b4
Lynn <lynnnlam0723@gmail.com>
2023-04-04 20:03:41 -0400

    fixed nationality search

--- HW7.py
+++ HW7.py
@@ -77,9 +77,11 @@ def make_players_table(data, cur, conn):
         # the player's name, their position_id, and their nationality.
 
 def nationality_search(countries, cur, conn):
+    players = list()
     for country in countries:
         cur.execute("SELECT name, position_id, nationality FROM Players WHERE nationality = ?", country)
-    conn.commit()
+        players.append(cur.fetchall())
+    return players
 
 ## [TASK 3]: 10 points
 # finish the function birthyear_nationality_search
